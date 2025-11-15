b06a1cf7a644cd82846edd8f563b2f3f4d8f367e
denilany <denilany1@gmail.com>
2025-11-15 11:24:54 +0300

    Hide footer on login/signup pages with conditional rendering

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import ConditionalFooter from "../components/ConditionalFooter";
 import { ThemeProvider } from "../contexts/ThemeContext";
 
 export const metadata: Metadata = {
@@ -22,7 +22,7 @@ export default function RootLayout({
           <main className="flex-grow">
             {children}
           </main>
-          <Footer />
+          <ConditionalFooter />
         </ThemeProvider>
       </body>
     </html>
