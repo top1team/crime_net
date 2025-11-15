67e95b8d1cbf13c02fd1c8bef621deb569b1d5f1
denilany <denilany1@gmail.com>
2025-11-15 12:07:09 +0300

    Add dark theme backgrounds to auth pages

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "../../components/AuthForm";
 
 export default function SignupPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="min-h-screen bg-stone-50 dark:bg-stone-900 flex items-center justify-center p-4">
       <AuthForm initialState="signup" />
     </div>
   );
